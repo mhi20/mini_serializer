0d28ab2c222237fa11e166e108f6af7a8426d099
mahdi <mahdializadeh20@gmail.com>
2020-02-23 18:16:56 +0330

    Fix code style

--- lib/mini_serializer.rb
+++ lib/mini_serializer.rb
@@ -3,86 +3,66 @@ require "mini_serializer/version"
 module MiniSerializer
     class Serializer
 
-      def initialize(object_main,except_params={})
-        @wams_has_one=[]
-        @wams_has_many=[]
-        @wams_association_object=[]
-        @wams_association_for_to_json={}
-        @wams_main_object=object_main
-        @wams_foreign_object={}
-        if !except_params.empty?
-          @wams_except_params_main_object=except_params[:except]
-        else
-          @wams_except_params_main_object={}
-        end
-
+      def initialize(object_main, except_params = {})
+        @wams_has_one = []
+        @wams_has_many = []
+        @wams_association_object = []
+        @wams_association_for_to_json = {}
+        @wams_main_object = object_main
+        @wams_foreign_object = {}
+        @wams_except_params_main_object = except_params[:except] || {}
       end
 
-      def add_has_many(object,except_methods=[])
-        wams_has_many<< [object,except_methods]
+      def add_has_many(object, except_methods = [])
+        wams_has_many << [object, except_methods]
       end
 
-      def add_has_one(object,except_methods=[])
-        wams_has_one<<[object,except_methods]
+      def add_has_one(object, except_methods = [])
+        wams_has_one << [object, except_methods]
       end
 
-      def get_params_included_to_json(with_except:false)
+      def get_params_included_to_json(with_except: false)
         fetch_association_objects
-        if with_except
-          {include:wams_association_for_to_json,except:wams_except_params_main_object}
-        else
-          {include:wams_association_for_to_json}
-        end
+        return { include: wams_association_for_to_json, except: wams_except_params_main_object } if with_except
+
+        { include: wams_association_for_to_json }
       end
 
-      def add_foreign_object(objects,custom_name,include_params,except_params=[]) # ezafe kardane object khareji be object asli
+      def add_foreign_object(objects, custom_name, include_params, except_params = []) # ezafe kardane object khareji be object asli
 
         if include_params.any?
           if include_params.kind_of?(Hash)
-            param_for_include=include_params.keys
+            param_for_include = include_params.keys
           elsif (include_params.kind_of?(Array))
-
-            param_for_include=include_params
+            param_for_include = include_params
           end
-          objects=objects.includes(param_for_include)
+          objects = objects.includes(param_for_include)
         end
 
         if except_params.empty?
-          wams_foreign_object[custom_name]=objects
+          wams_foreign_object[custom_name] = objects
         else
-          wams_foreign_object[custom_name]=objects.as_json(except:except_params,include:include_params)
+          wams_foreign_object[custom_name] = objects.as_json(except: except_params, include: include_params)
         end
 
       end
 
-      def json_serializer(object=nil)
-        if object.nil?
-          included_data=includes_to_serializer
-        end
+      def json_serializer(object = nil)
+        included_data = object || includes_to_serializer
 
-        if !wams_except_params_main_object.empty? # have except params for main object
-
-          if !object.nil? # have object for proccess
-            response_data= object.as_json({include:wams_association_for_to_json,except:wams_except_params_main_object})
-          else # haven't object for proccess
-            response_data= included_data.as_json({include:wams_association_for_to_json,except:wams_except_params_main_object})
-          end
-
+        if wams_except_params_main_object.any? # have except params for main object
+          response_data = included_data.as_json(include: wams_association_for_to_json, except: wams_except_params_main_object)
         else
-
-          if !object.nil?
-            response_data= object.as_json({include:wams_association_for_to_json})
-          else
-            response_data= included_data.as_json({include:wams_association_for_to_json})
-          end
+          response_data = included_data.as_json(include: wams_association_for_to_json)
         end
+
         if wams_foreign_object.any? # when use method foreign_object
           if wams_main_object.kind_of?(Class)
             name_main_object=wams_main_object.to_s.downcase.pluralize
           else
             name_main_object=wams_main_object.klass.name.downcase.pluralize
           end
-          return {"#{name_main_object}": response_data,included:wams_foreign_object}
+          return {"#{name_main_object}": response_data, included: wams_foreign_object}
 
         else
           response_data
@@ -92,22 +72,21 @@ module MiniSerializer
       end
 
       def get_object_included
-        return includes_to_serializer
+        includes_to_serializer
       end
 
       private
-      attr_accessor :wams_has_many,:wams_has_one ,:wams_association_object,
-                    :wams_except_params,:wams_main_object,:wams_association_for_to_json,
-                    :wams_except_params_main_object,:wams_foreign_object
+
+      attr_accessor :wams_has_many, :wams_has_one, :wams_association_object,
+                    :wams_except_params, :wams_main_object, :wams_association_for_to_json,
+                    :wams_except_params_main_object, :wams_foreign_object
 
       def includes_to_serializer
-        data_with_includes=nil
+        data_with_includes = nil
         fetch_association_objects
 
-        if wams_has_many.any?
+        data_with_includes = wams_main_object.includes(wams_association_object) if wams_has_many.any?
 
-          data_with_includes= wams_main_object.includes(wams_association_object)
-        end
         if wams_has_one.any?
           data_with_includes.merge! wams_main_object.includes(wams_association_object)
         end
@@ -116,18 +95,18 @@ module MiniSerializer
           return wams_main_object # return main object
         end
 
-        return data_with_includes
+        data_with_includes
       end
 
       def fetch_association_objects
-        wams_has_many.map do  |object_asoc,except_params|
-          wams_association_for_to_json[object_asoc]={except: except_params} # for to_json hash
-          wams_association_object<<object_asoc # for included (relation record)
+        wams_has_many.each do |object_asoc, except_params|
+          wams_association_for_to_json[object_asoc] = { except: except_params } # for to_json hash
+          wams_association_object << object_asoc # for included (relation record)
         end
 
-        wams_has_one.map do |object_asoc,except_params|
-          wams_association_for_to_json[object_asoc]={except: except_params} # for to_json hash
-          wams_association_object<<object_asoc # for included (relation record)
+        wams_has_one.each do |object_asoc, except_params|
+          wams_association_for_to_json[object_asoc] = { except: except_params } # for to_json hash
+          wams_association_object << object_asoc # for included (relation record)
         end
       end
 
